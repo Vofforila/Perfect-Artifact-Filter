385ba4fb438c61115ae27053a24d858db95a82f8
Vofforila <polberci@yahoo.ro>
2024-12-09 02:09:10 +0200

    Finished Archive

--- libs/gi/page-archive/src/index.tsx
+++ libs/gi/page-archive/src/index.tsx
@@ -1,115 +1,121 @@
-import { AnvilIcon } from '@genshin-optimizer/common/svgicons'
+import { useDataEntryBase } from '@genshin-optimizer/common/database-ui'
 import { CardThemed } from '@genshin-optimizer/common/ui'
-import { FlowerIcon } from '@genshin-optimizer/gi/svgicons'
-import { People } from '@mui/icons-material'
-import { Box, CardContent, Grid, Tab } from '@mui/material'
-import type { ReactElement } from 'react'
-import { useMemo } from 'react'
-import { useTranslation } from 'react-i18next'
-import { useMatch } from 'react-router-dom'
+import type { SortConfigs } from '@genshin-optimizer/common/util'
+import { sortFunction } from '@genshin-optimizer/common/util'
+import type { CharacterKey } from '@genshin-optimizer/gi/consts'
+import { allCharacterKeys } from '@genshin-optimizer/gi/consts'
+import { useDatabase } from '@genshin-optimizer/gi/db-ui'
+import { i18n } from '@genshin-optimizer/gi/i18n'
+import { getCharEle, getCharStat } from '@genshin-optimizer/gi/stats'
+import { LocationName, SillyContext } from '@genshin-optimizer/gi/ui'
+import {
+  Box,
+  CardContent,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+} from '@mui/material'
+import { useContext, useDeferredValue, useMemo, useState } from 'react'
 import perfect_sets from './artifacts.json'
 import PerfectartifactCard from './PerfectArtifactCard'
-import { PerfectArtifactSet } from './TestPerfectArtifacts'
-import type {
-  CharacterKey,
-} from '@genshin-optimizer/gi/consts'
-import {
-  CharacterName,
-  SillyContext,
-  iconAsset,
-} from '@genshin-optimizer/gi/ui'
-import { sortFunction } from '@genshin-optimizer/common/util'
-import { i18n } from '@genshin-optimizer/gi/i18n'
+import type { PerfectArtifactSet } from './TestPerfectArtifacts'
 
+const rarties = [5, 4] as const
+export default function PageArchive() {
+  const { silly } = useContext(SillyContext)
+  const database = useDatabase()
+  const archive = useDataEntryBase(database.displayArchive)
+  const [searchTerm, setSearchTerm] = useState('')
+  const searchTermDeferred = useDeferredValue(searchTerm)
 
+  const { character } = archive
 
+  const charKeys = useMemo(() => {
+    return allCharacterKeys.filter((cKey) => {
+      const { rarity, weaponType } = getCharStat(cKey)
+      if (!character.rarity.includes(rarity as (typeof rarties)[number]))
+        return false
+      if (!character.weaponType.includes(weaponType)) return false
 
-type Tab = {
-  i18Key: string
-  icon: ReactElement
-  value: string
-  to: string
-}
-const artifacts: Tab = {
-  i18Key: 'tabs.artifacts',
-  icon: <FlowerIcon />,
-  value: 'artifacts',
-  to: '/artifacts',
-}
-const weapons: Tab = {
-  i18Key: 'tabs.weapons',
-  icon: <AnvilIcon />,
-  value: 'weapons',
-  to: '/weapons',
-}
-
-const characters: Tab = {
-  i18Key: 'tabs.characters',
-  icon: <People />,
-  value: 'characters',
-  to: '/characters',
-}
-
-const tabs = [artifacts, weapons, characters] as const
-const tabValues = tabs.map(({ value }) => value)
-
-const sortedCharKeys = useMemo(
-  () =>
-    sortFunction([character.sortOrderBy], character.sortOrder === 'asc', {
-      name: (cKey: CharacterKey) =>
+      const nameStr = i18n.t(`charNames_gen:${cKey}`)
+      const sillyStr =
         silly && i18n.exists(`sillyWisher_charNames:${cKey}`)
           ? i18n.t(`sillyWisher_charNames:${cKey}`)
-          : i18n.t(`charNames_gen:${cKey}`),
-      rarity: (cKey: CharacterKey) => getCharStat(cKey).rarity,
-      element: (cKey: CharacterKey) => getCharEle(cKey),
-      type: (cKey: CharacterKey) => getCharStat(cKey).weaponType,
-    } as SortConfigs<SortKey, CharacterKey>),
-  [character.sortOrder, character.sortOrderBy, silly]
-)
+          : ''
+      if (
+        searchTermDeferred &&
+        !nameStr
+          .toLocaleLowerCase()
+          .includes(searchTermDeferred.toLocaleLowerCase()) &&
+        !sillyStr
+          .toLocaleLowerCase()
+          .includes(searchTermDeferred.toLocaleLowerCase())
+      )
+        return false
+      return true
+    })
+  }, [character, searchTermDeferred, silly])
 
+  type SortKey = 'name' | 'rarity' | 'element' | 'type'
+  const sortedCharKeys = useMemo(
+    () =>
+      sortFunction([character.sortOrderBy], character.sortOrder === 'asc', {
+        name: (cKey: CharacterKey) =>
+          silly && i18n.exists(`sillyWisher_charNames:${cKey}`)
+            ? i18n.t(`sillyWisher_charNames:${cKey}`)
+            : i18n.t(`charNames_gen:${cKey}`),
+        rarity: (cKey: CharacterKey) => getCharStat(cKey).rarity,
+        element: (cKey: CharacterKey) => getCharEle(cKey),
+        type: (cKey: CharacterKey) => getCharStat(cKey).weaponType,
+      } as SortConfigs<SortKey, CharacterKey>),
+    [character.sortOrder, character.sortOrderBy, silly]
+  )
+  const charKeysToShow = useMemo(() => charKeys.sort(sortedCharKeys))
 
-export default function PageArchive() {
-  const { t } = useTranslation('ui')
-
-  const {
-    params: { tab: tabRaw },
-  } = useMatch({ path: '/archive/:tab', end: false }) ?? {
-    params: {},
-  }
-  const tab = useMemo(() => {
-    const tab = tabValues.find((tv) => tv === tabRaw)
-    return tab ?? 'artifacts'
-  }, [tabRaw])
+  const [characterFilter, setCharacterFilter] = useState<string>('')
 
   return (
     <CardThemed>
       <CardContent>
-
- {/* Dropdown to select character, populated from CharacterKeys array */}
- {/* <select onChange={(e) => setCharacterFilter(e.target.value)} value={characterFilter}>
-        {CharacterKeys.map((character) => (
-          <option key={character} value={character}>
-            {character}
-          </option>
-        ))}
-      </select> */}
-  <CharacterName characterKey={cKey} gender={gender} />
+        <FormControl fullWidth>
+          <InputLabel>Character</InputLabel>
+          <Select
+            value={characterFilter}
+            onChange={(e) => setCharacterFilter(e.target.value)}
+            label="Character"
+          >
+            <MenuItem value="">None</MenuItem>
+            {charKeysToShow.map((character) => (
+              <MenuItem key={character} value={character}>
+                <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                  <LocationName location={character} />
+                </Box>
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
 
         <Grid container spacing={2}>
-          {(perfect_sets as PerfectArtifactSet[]).map((perfect_set) => {
-            let character = perfect_set.character
-            return (
-              <Grid item xs={6}>
-                <CardThemed>
-                  <CardContent>
-                    <Box sx={{ position: 'relative' }}>
-                      <PerfectartifactCard perfect_set={perfect_set} />
-                    </Box>
-                  </CardContent>
-                </CardThemed>
-              </Grid>
-            )
-          })}
+          {(perfect_sets as PerfectArtifactSet[])
+            .filter((perfect_set) => {
+              if (characterFilter === '') return true
+              return perfect_set.character === characterFilter
+            })
+            .map((perfect_set, index) => {
+              return (
+                <Grid item xs={6} key={index}>
+                  <CardThemed>
+                    <CardContent>
+                      <Box sx={{ position: 'relative' }}>
+                        <PerfectartifactCard perfect_set={perfect_set} />
+                      </Box>
+                    </CardContent>
+                  </CardThemed>
+                </Grid>
+              )
+            })}
         </Grid>
       </CardContent>
     </CardThemed>
