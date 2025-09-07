8e55f8e561bfac3614439d6ac7cb35e53e3f1c94
Vofforila <vofforila@gmail.com>
2025-09-07 21:18:35 +0300

    Updates

---
+++ assets/Upload-BxSruv67.js
@@ -0,0 +1,1 @@
+import{v as a,w as r,j as t}from"./index-CLWYZhSe.js";var e={},o=r;Object.defineProperty(e,"__esModule",{value:!0});var u=e.default=void 0,v=o(a()),d=t;u=e.default=(0,v.default)((0,d.jsx)("path",{d:"M5 20h14v-2H5zm0-10h4v6h6v-6h4l-7-7z"}),"Upload");export{u as d};
